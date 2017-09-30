f1f2aa1cb1341063acd803d0405f80ae829abda1
alexolivier <alex@alexolivier.me>
2017-09-30 23:48:46 +0100

    bigger machine and sleep

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,17 @@ gcloud container clusters create aok8s \
   --enable-autoupgrade \
   --enable-cloud-monitoring \
   --enable-cloud-logging \
-  --machine-type=g1-small \
+  --machine-type=n1-standard-1 \
   --num-nodes=2 \
   --scopes bigquery,storage-rw,monitoring,sql,sql-admin,default \
   --preemptible \
   --zone=europe-west1-b;
 gcloud container clusters get-credentials aok8s;
 helm init;
+echo "Wait 30s for tiller to start";
+sleep 30;
 helm install --name ing --namespace kube-system --values traefik-values.yaml stable/traefik;
-echo "NOW REMOVE THE EMBEDDED CERTS"
+echo "NOW REMOVE THE EMBEDDED CERTS";
 kubectl edit configmap ing-traefik -o yaml --namespace kube-system;
 kubectl scale deployment/ing-traefik --replicas=0 --namespace kube-system; 
 kubectl scale deployment/ing-traefik --replicas=1 --namespace kube-system;
